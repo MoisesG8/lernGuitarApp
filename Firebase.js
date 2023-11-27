fadf2dbc16444eedb8cfb9ce64fc57fa42ed224e
Moises Gonzalez <52986690+MoisesG8@users.noreply.github.com>
2023-11-27 16:56:50 -0600

    Update Firebase.js
    Eliminación de credenciales firebase

--- Firebase.js
+++ Firebase.js
@@ -4,12 +4,12 @@ import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAmDfvq7PIyUaS-4qKYeoRjBV6HWUpR6wE",
-    authDomain: "learnguitarapp-7e65b.firebaseapp.com",
-    projectId: "learnguitarapp-7e65b",
-    storageBucket: "learnguitarapp-7e65b.appspot.com",
-    messagingSenderId: "1089450524082",
-    appId: "1:1089450524082:web:9e4eac5f9b1671b5c378ab"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
 };
 
 // Initialize Firebase
